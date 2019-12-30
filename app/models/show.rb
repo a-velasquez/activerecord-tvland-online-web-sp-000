7bbb90f6f9ceb367c11b224603ff69ade90dff6d
Alex Velasquez <jtinnesco@gmail.com>
2019-12-30 04:17:20 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,9 @@ class Show < ActiveRecord::Base
   belongs_to :network
   has_many :Actors, through: :characters
 
+  def actors_list
+    self.actors.collect do |actor|
+      actor.full_name
+    end
+
 end
